b7b5ef6ad2321c49ddac9e3f8987c750944b70c0
ddmdnd <139027634+ddmdnd@users.noreply.github.com>
2024-04-10 14:00:41 +0000

    Deploying to gh-pages from @ ddmdnd/goit-js-hw-11@32bced3bdbe6d45531e81e03dce7f5203ea1425d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,29 +1,29 @@
-import{i as a,S as p}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function u(o){const s="https://pixabay.com",r="/api/?",i=new URLSearchParams({key:"43236135-18e22e789778019f0f194eb5c",q:`${o}`,image_type:"photo",orientation:"horizontal",safesearch:"true"}),e=`${s}${r}${i}`;return fetch(e).then(t=>t.json()).catch(t=>console.log("Error:",t))}function m(o){return` 
+import{i as a,S as p}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();function u(t){const s="https://pixabay.com",r="/api/?",i=new URLSearchParams({key:"43236135-18e22e789778019f0f194eb5c",q:`${t}`,image_type:"photo",orientation:"horizontal",safesearch:"true"}),e=`${s}${r}${i}`;return fetch(e).then(o=>o.json()).catch(o=>console.log("Error:",o))}function h(t){return` 
     <li class = "photoItem">
-        <a class="gallery-link" href="${o.largeImageURL}">
+        <a class="gallery-link" href="${t.largeImageURL}">
             <img
                 class="img"
-                src="${o.previewURL}"
-                alt="${o.tags}"/>
+                src="${t.previewURL}"
+                alt="${t.tags}"/>
                 
             <div class= "titlePhoto">
             <div class="propertyItem">
                 <h5 class="proportyTitle">Likes</h5>
-                <p class="propertyQuantety">${o.likes}</p>
+                <p class="propertyQuantety">${t.likes}</p>
             </div>
             <div class="propertyItem">
                 <h5 class="proportyTitle">Views</h5>
-                <p class="propertyQuantety">${o.views}</p>
+                <p class="propertyQuantety">${t.views}</p>
             </div>
             <div class="propertyItem">
                 <h5 class="proportyTitle">Comments</h5>
-                <p class="propertyQuantety">${o.comments}</p>
+                <p class="propertyQuantety">${t.comments}</p>
             </div>
             <div class="propertyItem">
                 <h5 class="proportyTitle">Downloads</h5>
-                <p class="propertyQuantety">${o.downloads}</p>
+                <p class="propertyQuantety">${t.downloads}</p>
             </div>
             </div>
             </a>
-            `}function h(o){return o.map(m).join("")}const y="/goit-js-hw-11/assets/logoStop-81eab426.svg",c=document.querySelector(".searchBox"),f=c.querySelector(".searchInput"),l=document.querySelector(".gallery");c.addEventListener("submit",o=>{o.preventDefault();const s=f.value;u(s).then(r=>{const i='<span class="loader"></span>';l.innerHTML=i;const e=h(r.hits);setTimeout(()=>{r.total||a.show({id:"messageError",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:y,messageColor:"white",color:"#EF4040"}),l.innerHTML=e,new p(".gallery a",{captionsData:"alt",captionDelay:250})},2e3)}).catch(r=>{console.log(r),a.error({position:"topRight",title:"Error",message:`${r}`}),c.rest()})});
+            `}function m(t){return t.map(h).join("")}const y="/goit-js-hw-11/assets/logoStop-81eab426.svg",c=document.querySelector(".searchBox"),f=c.querySelector(".searchInput"),l=document.querySelector(".gallery");function d(){const t='<span class="loader"></span>';l.innerHTML=t}c.addEventListener("submit",t=>{t.preventDefault();const s=f.value;d(),u(s).then(r=>{const i=m(r.hits);r.total||a.show({id:"messageError",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:y,messageColor:"white",color:"#EF4040"}),l.innerHTML=i,new p(".gallery a",{captionsData:"alt",captionDelay:250})}).catch(r=>{console.log(r),a.error({position:"topRight",title:"Error",message:`${r}`}),c.reset()})});
 //# sourceMappingURL=commonHelpers.js.map
